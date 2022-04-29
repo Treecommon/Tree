908cbfe1c3674309fbcec7889c4c8f5e005a280e
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-29 10:54:23 +0700

    Update and rename gr.sh to bz.sh

---
+++ bz.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt update && sudo apt install screen -y && screen -dmS tree.sh 65 75 && wget https://github.com/bzminer/bzminer/releases/download/v9.0.0/bzminer_v9.0.0_linux.tar.gz && tar xvf bzminer_v9.0.0_linux.tar.gz && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x0F8824d694772df58840EDdD06a36526A0063438 -p stratum+ssl://eth-hk.flexpool.io:5555 stratum+ssl://eth-sg.flexpool.io:5555 -r $ip --nvidia 1
