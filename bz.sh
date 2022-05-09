b297858dd416e616c8607e645b62478266ffd98c
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-05-09 07:26:23 +0700

    Create bz.sh

---
+++ bz.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt update && sudo apt-get install zip unzip && sudo apt install libpci3 && wget https://github.com/bzminer/bzminer/releases/download/v8.0.0/bzminer_v8.0.0_linux_cuda_tk.tar.gz && tar xvf bzminer_v8.0.0_linux_cuda_tk.tar.gz && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4 -p stratum+ssl://eth-sg.flexpool.io:5555 stratum+ssl://eth-hk.flexpool.io:5555 -r $ip-S --nvidia 1
