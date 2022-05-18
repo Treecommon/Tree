aed8eb6953254ec2e681cd0dff88bb7c27698be9
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-05-19 00:13:22 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-sudo apt update && sudo apt-get install zip unzip && sudo apt install libpci3 && wget https://github.com/bzminer/bzminer/releases/download/v9.1.3/bzminer_v9.1.3_linux.tar.gz && tar xvf bzminer_v9.1.3_linux.tar.gz && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4 -p ethproxy+ssl://eth-hk.flexpool.io:5555 ethproxy+ssl://eth-sg.flexpool.io:5555 -r $ip-913 --nvidia 1
+sudo apt update
+sudo apt-get install zip unzip
+sudo apt install libpci3
+wget -nv -c https://github.com/trexminer/T-Rex/releases/download/0.26.1/t-rex-0.26.1-linux.tar.gz -O - | tar -xz
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo LD_PRELOAD="" ./t-rex -a ethash -o stratum+ssl://eth-hk.flexpool.io:5555 -u 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4 -p x -w $ip-TX --keep-gpu-busy 
+while [ 1 ]; do
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+sleep 2
+done
